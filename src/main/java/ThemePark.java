d6af0d70a67514f09b427d9f845c30a7ed84ccb2
Athina Gouseti <athina.gouseti@outlook.com>
2021-11-15 22:18:24 +0000

    added homework, visitor test commented out

---
+++ src/main/java/ThemePark.java
@@ -0,0 +1,26 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import people.Visitor;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
+public class ThemePark {
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+    private ArrayList<IReviewed> reviewed;
+
+    public ThemePark(ArrayList<Attraction> attractions, ArrayList<Stall> stalls) {
+        this.attractions = attractions;
+        this.stalls = stalls;
+    }
+
+    public ArrayList<IReviewed> getAllReviewed(){
+        return this.reviewed;
+    }
+
+    public void visit(Visitor visitor, Attraction attraction){
+        attraction.incrementVisitCount();
+        visitor.addAttraction(attraction);
+    }
+}
